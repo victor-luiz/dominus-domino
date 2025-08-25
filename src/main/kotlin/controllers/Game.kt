843006ceb2d3ba068f30c2df05bbae4049c6716c
victor luiz <luizvic.2112@gmail.com>
2025-08-25 12:08:54 -0300

    definindo o começo de jogo

--- src/main/kotlin/controllers/Game.kt
+++ src/main/kotlin/controllers/Game.kt
@@ -4,25 +4,22 @@ import models.Player
 import models.Tile
 
 class Game (playersName: List<String>) {
-    val allTiles = mutableListOf<Tile>()
-    val remnant = mutableListOf<Tile>()
-    val players = mutableListOf<Player>()
-    val game = ArrayDeque<Tile>()
+    private val allTiles = mutableListOf<Tile>()
+    private val remnant = mutableListOf<Tile>()
+    private val players = mutableListOf<Player>()
+    private val game = ArrayDeque<Tile>()
 
     init {
-        for (name in playersName) {
-            players.add(Player(name))
-        }
+        playersName.forEach { players.add(Player(it)) }
     }
 
     fun start() {
-        println("--- Iniciando o Jogo de Dominó ---")
-        createTiles()
-        shuffleAndDealTiles()
-        for (player in players) {
-            println(player.toString())
-        }
-        println("Pedras restantes ${remnant.size}")
+        headGame()
+        var indexCurrentPlayer = players.indexOf(initalGame())
+        indexCurrentPlayer = nextPlayer(indexCurrentPlayer)
+        println(game.toString())
+        println("----------------------\n")
+        println("Jogador atual: ${players[indexCurrentPlayer].name}")
     }
 
     private fun createTiles() {
@@ -43,5 +40,45 @@ class Game (playersName: List<String>) {
                 player.addTile(remnant.removeFirst());
             }
         }
+    }
+
+    private fun headGame() {
+        println("--- Iniciando Dominó ---")
+        createTiles()
+        shuffleAndDealTiles()
+        println("\n--- Mãos Iniciais ---")
+        players.forEach { println(it) }
+        println("----------------------\n")
+    }
+
+    private fun initalGame(): Player? {
+        val initialPlayer = findPlayerInitial()
+        if (initialPlayer == null) {
+            println("ERRO: Não foi possível determinar o jogador inicial.")
+            return null
+        }
+
+        val (playerInitial, tileInitial) = initialPlayer
+        playerInitial.throwTile(tileInitial)
+        game.addFirst(tileInitial)
+
+        return playerInitial;
+    }
+
+    private fun findPlayerInitial(): Pair<Player, Tile>? {
+        for (numberDoble in 6 downTo 0) {
+            val tileDoble = Tile(numberDoble, numberDoble);
+            for (player in players) {
+                if (player.hand.contains(tileDoble)) {
+                    println("Jogador inicial encontrado: ${player.name} com a peça $tileDoble")
+                    return Pair(player, tileDoble);
+                }
+            }
+        }
+        return null
+    }
+
+    private fun nextPlayer(index: Int): Int {
+        return (index + 1) % players.size
     }
 }
